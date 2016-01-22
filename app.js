35896f0e89914ee73e50520dc90cffdd4af0071a
Konrad <konrad@truekings.de>
2016-01-22 15:42:10 +1100

    cleaned

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 var express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-var exphbs  = require('express-handlebars');
-
-
-var routes = require('./routes/index');
-var users = require('./routes/user');
+var keypress = require('keypress');
 
 var app = express();
 
-var env = process.env.NODE_ENV || 'development';
-app.locals.ENV = env;
-app.locals.ENV_DEVELOPMENT = env == 'development';
-
-var keypress = require('keypress');
  // make `process.stdin` begin emitting "keypress" events 
 keypress(process.stdin);
  
@@ -30,59 +16,6 @@ process.stdin.on('keypress', function (ch, key) {
  
 process.stdin.setRawMode(true);
 process.stdin.resume();
-
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  partialsDir: ['views/partials/']
-}));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
-
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
-});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
